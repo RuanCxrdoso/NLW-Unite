dab9f479251eea98d68ea542c0086ca760e480a5
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-05-29 15:37:53 -0300

    feat: back end url

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -49,7 +49,7 @@ export function AttendeeList() {
   const pagesNumber = Math.ceil(total / 10)
 
   useEffect(() => {
-    const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+    const url = new URL('https://nlw-unite-db.onrender.com/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
 
     url.searchParams.set('pageIndex', String(page - 1))
 
